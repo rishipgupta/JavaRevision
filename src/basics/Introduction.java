108e9134245d5ea82575571fe6a711ab7f3bb3bc
Riship Gupta <guptariship@gmail.com>
2022-11-01 15:32:32 +0530

    Examples of State and Behaviour

---
+++ src/basics/Introduction.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class Introduction {
+    //Examples of State and Behaviour
+
+    //State:
+    String name;
+    int age;
+    double salary;
+
+    //Behaviours
+    void incrementAge(){
+        //code body
+    }
+
+    void printName(){
+        //code body
+    }
+
+}
